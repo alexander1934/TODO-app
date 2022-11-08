e4cce2e5158113bf48ab3abd36d9a39fdf4f5824
Alexander Kovalenko <alexankovalenko23@gmail.com>
2022-11-08 17:22:29 +0300

    added add button and remove button

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-// function generateHashtag(str) {
-//   let strSplit = str.split(" ");
-//   console.log(strSplit);
-//   function capitalizeWords(element) {
-//     return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
-//   }
-//   let result = strSplit.map(capitalizeWords);
-//   console.log(result);
-//   let finishResult = result.join("");
-//   return "#" + finishResult;
-// }
+let text = document.querySelector(".input__field");
+let addButton = document.querySelector(".input__button");
+let todo = document.querySelector(".todo");
+let deleteButton = document.querySelector(".button-block__item_delete");
 
-// generateHashtag("Hello world");
+// Добавление пункта
 
-function generateHashtag(str) {
-  if (str =='' || str.length > 140){
-    return false;
-  }
-  const stringWithCamelCase = str
-  .split(' ')
-  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-  .join('');
-  return "#" + stringWithCamelCase;
+addButton.addEventListener("click", addItem);
+
+function addItem(){
+  let valueHTML = `<div class="todo__item">
+  <p class="todo__p">${text.value}</p>
+  <div class="button-block">
+      <button class="button-block__item_completed" data-action="completed"></button>
+      <button class="button-block__item_delete" data-action="delete">
+          <svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M22,5H17V2a1,1,0,0,0-1-1H8A1,1,0,0,0,7,2V5H2A1,1,0,0,0,2,7H3.061L4,22.063A1,1,0,0,0,5,23H19a1,1,0,0,0,1-.937L20.939,7H22a1,1,0,0,0,0-2ZM9,3h6V5H9Zm9.061,18H5.939L5.064,7H18.936ZM9,11v6a1,1,0,0,1-2,0V11a1,1,0,0,1,2,0Zm4,0v6a1,1,0,0,1-2,0V11a1,1,0,0,1,2,0Zm3-1a1,1,0,0,1,1,1v6a1,1,0,0,1-2,0V11A1,1,0,0,1,16,10Z"/>
+          </svg>
+      </button>
+  </div>
+</div>`;
+  todo.insertAdjacentHTML('beforeend', valueHTML)
+  text.value = "";
+  text.focus();
 }
 
-console.log(generateHashtag(""))
+// Удаление пункта
+
+todo.addEventListener('click', removeItem);
+
+function removeItem(event) {
+  if (event.target.dataset.action === 'delete'){
+    let parent = event.target.closest('.todo__item')
+    parent.remove()
+  }
+}
