bbe081d35b54d3f37c519628abd6a2f19f26bd6a
Alexander Kovalenko <alexankovalenko23@gmail.com>
2022-11-17 20:33:16 +0300

    added saving in local storage

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
+//Поиск элементов на странице
 let text = document.querySelector(".input__field");
 let addButton = document.querySelector(".input__button");
 let todo = document.querySelector(".todo");
 
-// Добавление пункта
+//  Массив с задачами и манипуляция с localStorage
+let tasks = [];
 
-addButton.addEventListener("click", addItem);
+if (localStorage.getItem("tasks")) {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+  tasks.forEach((task) => {
+    const status = task.done ? "todo__p todo__p_strike" : "todo__p";
 
+    let valueHTML = `<div class="todo__item" id="${task.id}">
+    <p class="${status}">${task.text}</p>
+    <div class="button-block">
+        <button class="button-block__item_completed" data-action="completed"></button>
+        <button class="button-block__item_delete" data-action="delete">
+            <svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M22,5H17V2a1,1,0,0,0-1-1H8A1,1,0,0,0,7,2V5H2A1,1,0,0,0,2,7H3.061L4,22.063A1,1,0,0,0,5,23H19a1,1,0,0,0,1-.937L20.939,7H22a1,1,0,0,0,0-2ZM9,3h6V5H9Zm9.061,18H5.939L5.064,7H18.936ZM9,11v6a1,1,0,0,1-2,0V11a1,1,0,0,1,2,0Zm4,0v6a1,1,0,0,1-2,0V11a1,1,0,0,1,2,0Zm3-1a1,1,0,0,1,1,1v6a1,1,0,0,1-2,0V11A1,1,0,0,1,16,10Z"/>
+            </svg>
+        </button>
+    </div>
+  </div>`;
+    todo.insertAdjacentHTML("beforeend", valueHTML);
+  });
+}
+
+// Прослушка на Enter
 text.addEventListener("keypress", (event) => {
-  if (event.key === "Enter"){
+  if (event.key === "Enter") {
     addItem();
   }
-})
+});
 
-function addItem(){
-  if (text.value === ""){
-    return
+// Добавление пункта
+addButton.addEventListener("click", addItem);
+
+// Удаление пункта
+todo.addEventListener("click", removeItem);
+
+// Зачеркивание выполненной задачи
+todo.addEventListener("click", crossOutItem);
+
+//Функции
+function addItem() {
+  if (text.value === "") {
+    return;
   }
-  let valueHTML = `<div class="todo__item">
-  <p class="todo__p">${text.value}</p>
+
+  // обьект задачи
+  const item = {
+    id: Date.now(),
+    text: text.value,
+    done: false,
+  };
+
+  tasks.push(item);
+
+  const status = item.done ? "todo__p todo__p_strike" : "todo__p";
+
+  let valueHTML = `<div class="todo__item" id="${item.id}">
+  <p class="${status}">${item.text}</p>
   <div class="button-block">
       <button class="button-block__item_completed" data-action="completed"></button>
       <button class="button-block__item_delete" data-action="delete">
@@ -27,30 +70,52 @@ function addItem(){
       </button>
   </div>
 </div>`;
-  todo.insertAdjacentHTML('beforeend', valueHTML);
+  todo.insertAdjacentHTML("beforeend", valueHTML);
   text.value = "";
   text.focus();
+
+  saveToLocalStorage();
 }
-
-// Удаление пункта
-
-todo.addEventListener('click', removeItem);
 
 function removeItem(event) {
-  if (event.target.dataset.action === 'delete'){
-    let parent = event.target.closest('.todo__item')
-    parent.remove()
+  if (event.target.dataset.action === "delete") {
+    let parent = event.target.closest(".todo__item");
+
+    const index = tasks.findIndex((task) => {
+      if (parent.id == task.id) {
+        return true;
+      }
+    });
+
+    //Удаление из элемента из массива
+    tasks.splice(index, 1);
+
+    parent.remove();
+
+    saveToLocalStorage();
   }
 }
 
-// Зачеркивание выполненной задачи
-
-todo.addEventListener('click', crossOutItem);
-
-function crossOutItem (event) {
-  if (event.target.dataset.action === 'completed') {
+function crossOutItem(event) {
+  if (event.target.dataset.action === "completed") {
     let parent = event.target.closest(".todo__item");
     let p = parent.querySelector(".todo__p");
+
+    // Найти ID
+    const item = tasks.find((task) => {
+      if (task.id == parent.id) {
+        return true;
+      }
+    });
+
+    item.done = !item.done;
+
     p.classList.toggle("todo__p_strike");
+
+    saveToLocalStorage();
   }
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
